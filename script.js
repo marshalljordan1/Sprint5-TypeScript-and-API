9e4d2726a69dbd92d77fedafb9a0a50c1bc81e8e
marshalljordan1 <jordan_marshall@live.co.uk>
2023-02-28 14:18:25 +0100

    complete ex 5 and ex 6

--- script.js
+++ script.js
@@ -10,10 +10,27 @@ const weather = document.getElementById("weather");
 // Create an empty array to store joke objects
 const reportJokes = [];
 
+// Create an array of APIs to alternate between 
+const jokeAPIs = ['https://icanhazdadjoke.com', 'https://api.chucknorris.io/jokes/random'];
+let currentAPI = 0;
+let jokeObj = {};
+
+const mainBlobsArray = ['blob-1', 'blob-2', 'blob-3', 'blob-4', 'blob-5'];
+
 // Function to fetch a joke and handle voting
 function nextJoke() {
+  const jokeBox = document.querySelector('.joke-box');
+    const randomBlob = mainBlobsArray[Math.floor(Math.random() * mainBlobsArray.length)];
+    jokeBox.classList.remove(...mainBlobsArray);
+    jokeBox.classList.add(randomBlob);
+
+    for (let i = 0; i < rateButtons.length; i++) {
+      rateButtons[i].style.display = 'block';
+    }
+  const apiUrl = jokeAPIs[currentAPI];
+  currentAPI = (currentAPI + 1) % jokeAPIs.length;
     // Call the API to get a joke
-    fetch('https://icanhazdadjoke.com', {
+    fetch(apiUrl, {
         headers: {
           'Accept': 'application/json'
         }
@@ -21,13 +38,23 @@ function nextJoke() {
         // Parse the data response as JSON
         .then(response => response.json())
         // Handle the data received
+
+        // Create a new joke object with a score of 0 and the current date in an ISO string if current API === 1 
         .then(data => {
-          // create a new joke object with a score of 0 and the current date in an ISO string
-          const jokeObj = {
-            joke: data.joke,
-            score: 0,
-            date: new Date().toISOString()
-          };
+
+          if (currentAPI === 1) {
+            jokeObj = {
+              joke: data.joke,
+              score: 0,
+              date: new Date().toISOString()
+            }
+          } else if (currentAPI === 0) {
+              jokeObj = {
+                joke: data.value,
+                score: 0,
+                date: new Date().toISOString()
+              };
+            };
           // Display the joke text on screen 
           showJoke.innerText = jokeObj.joke;
           // Add event listeners for each rating button 
@@ -50,19 +77,15 @@ function nextJoke() {
           else {
             reportJokes.push(jokeObj);
           }
+          console.log(reportJokes);
           // Log the contents of the reportJokes array to the console 
-          console.log(reportJokes)
         })
         // Handle any errors
         .catch(error => console.error(error));
       };
 
 // Add event listener to the show rateButtons when first joke is revealed 
-jokeButton.addEventListener('click', function() {
-    for (let i = 0; i < rateButtons.length; i++) {
-      rateButtons[i].style.display = 'block';
-    }
-});
+
   
 fetch('https://api.openweathermap.org/data/2.5/weather?q=Barcelona&appid=0db72ac48dddd8406450bfa9aba40aaf', {
   headers: {
@@ -71,7 +94,13 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=Barcelona&appid=0db72ac
 })
 .then(response => response.json())
 .then(data => {
+  const iconCode = data.weather[0].icon;
+  const weatherIcon = document.getElementById("weather-icon");
   const kelvinTemp = data.main.temp;
+  const temperatureElement = document.getElementById("degrees");
+  const iconUrl = `http://openweathermap.org/img/wn/${iconCode}.png`;
+    weatherIcon.innerHTML = `<img src="${iconUrl}" alt="Weather icon">`;
   const celsiusTemp = Math.floor(kelvinTemp - 273.15);
-  weather.innerText = `${celsiusTemp}°C`;
-});
+  temperatureElement.innerText = `${celsiusTemp}°C`;
+})
+
