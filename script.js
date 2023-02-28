23fa042fb2f07d5f1e967db12140eecad24b5885
marshalljordan1 <jordan_marshall@live.co.uk>
2023-02-28 14:58:03 +0100

    fix rate button reveal and comment each section

--- script.js
+++ script.js
@@ -15,18 +15,23 @@ const jokeAPIs = ['https://icanhazdadjoke.com', 'https://api.chucknorris.io/joke
 let currentAPI = 0;
 let jokeObj = {};
 
+// Create an array of class names for the background blobs
 const mainBlobsArray = ['blob-1', 'blob-2', 'blob-3', 'blob-4', 'blob-5'];
 
 // Function to fetch a joke and handle voting
 function nextJoke() {
+  // Select the HTML element with the class "joke-box" and add a random background blob from mainBlobsArray
   const jokeBox = document.querySelector('.joke-box');
     const randomBlob = mainBlobsArray[Math.floor(Math.random() * mainBlobsArray.length)];
     jokeBox.classList.remove(...mainBlobsArray);
     jokeBox.classList.add(randomBlob);
 
-    for (let i = 0; i < rateButtons.length; i++) {
-      rateButtons[i].style.display = 'block';
-    }
+    // Display the rating buttons
+    Array.from(rateButtons).forEach(button => {
+      button.style.display = 'block';
+    });
+
+  // Get the current joke API URL and increment the currentAPI variable
   const apiUrl = jokeAPIs[currentAPI];
   currentAPI = (currentAPI + 1) % jokeAPIs.length;
     // Call the API to get a joke
@@ -38,10 +43,8 @@ function nextJoke() {
         // Parse the data response as JSON
         .then(response => response.json())
         // Handle the data received
-
-        // Create a new joke object with a score of 0 and the current date in an ISO string if current API === 1 
         .then(data => {
-
+          // Create a new joke object with a score of 0 and the current date in an ISO string if current API === 1 
           if (currentAPI === 1) {
             jokeObj = {
               joke: data.joke,
@@ -57,6 +60,8 @@ function nextJoke() {
             };
           // Display the joke text on screen 
           showJoke.innerText = jokeObj.joke;
+          // Change button text to Next Joke
+          jokeButton.textContent = "Next Joke";
           // Add event listeners for each rating button 
           rateOne.addEventListener('click', () => {
             jokeObj.score = 1;
@@ -84,23 +89,32 @@ function nextJoke() {
         .catch(error => console.error(error));
       };
 
-// Add event listener to the show rateButtons when first joke is revealed 
-
-  
+// Fetch the weather data for Barcelona from the OpenWeatherMap API using the provided URL and API key  
 fetch('https://api.openweathermap.org/data/2.5/weather?q=Barcelona&appid=0db72ac48dddd8406450bfa9aba40aaf', {
-  headers: {
+// Set the headers to indicate that the response should be in JSON format  
+headers: {
     'Accept': 'application/json'
   }
 })
+// Parse the response data as JSON
 .then(response => response.json())
+// Handle the data received
 .then(data => {
+  // Get the weather icon code from the data received
   const iconCode = data.weather[0].icon;
+  // Get the HTML element for the weather icon
   const weatherIcon = document.getElementById("weather-icon");
+  // Get the temperature in Kelvin from the data received
   const kelvinTemp = data.main.temp;
+  // Get the HTML element for the temperature
   const temperatureElement = document.getElementById("degrees");
+  // Create the URL for the weather icon image using the icon code
   const iconUrl = `http://openweathermap.org/img/wn/${iconCode}.png`;
-    weatherIcon.innerHTML = `<img src="${iconUrl}" alt="Weather icon">`;
+  // Set the HTML inside the weather icon element to display the weather icon image
+  weatherIcon.innerHTML = `<img src="${iconUrl}" alt="Weather icon">`;
+  // Convert the temperature from Kelvin to Celsius and round down to the nearest integer
   const celsiusTemp = Math.floor(kelvinTemp - 273.15);
+  // Set the HTML inside the temperature element to display the temperature in Celsius
   temperatureElement.innerText = `${celsiusTemp}°C`;
 })
 
